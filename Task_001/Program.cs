05fdda314492c7e6246dabaa6848e35700e94cbd
Eugene_Pro <proskuryakov.e@list.ru>
2022-11-18 19:07:09 +0300

    Initial commit

---
+++ Task_001/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, используя метод. 
+// Если товары стоят больше или равно 1000 рублей, 
+//то мы их не покупаем. Стоимость товара задается рандомно
+//ответ необходимо предоставить в виде коронарного выражения 
+
+int priceCost = new Random().Next(10000);
+
+Console.WriteLine($"Цена товара: {priceCost}");
+
+// if (priceCost <= 1000)
+// {
+//     Console.WriteLine("Покупаем");
+// }
+// else
+// {
+//     Console.WriteLine("НЕ Покупаем");
+// }
+
+bool MaxPrice(int priceCost, int maxPrice)
+{
+    return priceCost <= maxPrice;
+}
+Console.WriteLine(MaxPrice(priceCost, 1000) ? "Покупаем" : "НЕ Покупаем");
---
+++ Task_002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найти максимальную цифру 
+// из рандомного трехзначного числа, используя метод 
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine($"Случайное число: {number}");
+
+int MaxDigit(int number)
+{
+int number1 = number / 100;
+int number2 = number % 100 / 10;
+int number3 = number % 10;
+int max = 0;
+
+if (number1 > max) max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+
+return max;
+}
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"максимальное число: {maxDigit}");
